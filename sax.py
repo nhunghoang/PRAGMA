494cbb3528ba71654c5cafdd8555383508b94eb0
rgbayrak <rozgns@gmail.com>
2020-04-17 14:41:23 -0500

    comments

--- sax.py
+++ sax.py
@@ -6,8 +6,6 @@ import hdf5storage
 import matplotlib.pyplot as plt
 import scipy.signal as ss
 
-
-
 mat = hdf5storage.loadmat('/home/bayrakrg/neurdy/d3/conn/processed_yeo_id108828.mat')
 conn = mat['Vp_clean'][0, 0]  #default is the 400 parcellation
 del mat
@@ -25,7 +23,6 @@ transformer = SymbolicAggregateApproximation(n_bins=8, strategy='uniform')
 # strategy='uniform': all bins in each sample have identical widths,
 # strategy='quantile': all bins in each sample have the same number of points,
 # strategy='normal': bin edges are quantiles from a standard normal distribution.
-data = []
 
 # make a letter dict
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -33,6 +30,7 @@ letter_dict = {}
 for i, l in enumerate(letter_list):
     letter_dict[l] = i
 
+data = []
 for i in range(conn_norm_ds.shape[0]):
     tmp_sax = transformer.transform(conn_norm_ds[i, :].reshape(1, -1))
     for j in range(tmp_sax.shape[1]):

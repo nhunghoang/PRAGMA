6dbcf44acfb9c749d95953313d6261ed3252f500
rgbayrak <rozgns@gmail.com>
2020-04-19 16:57:04 -0500

    added else statement to homogeneity

--- simple_server.py
+++ simple_server.py
@@ -18,11 +18,11 @@ CORS(app)
 reduced_ts = load_reduced_data(os.getcwd() + '/reduced_SID173839.txt')
 
 # functional conn data
-mat_filename = '/home/bayrakrg/neurdy/d3/server_data/processed_yeo_id108828.mat'  # Rubinov conn
+mat_filename = '../data/processed_yeo_id108828.mat'  # Rubinov conn
 # structural mapping data
-fatlas = '/home/bayrakrg/neurdy/d3/server_data/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_2mm.nii.gz'  # Shaefer atlas
-satlas = '/home/bayrakrg/neurdy/d3/server_data/mni_icbm152_t1_tal_nlin_asym_09c_seg_ds.nii.gz'  # SLANT atlas
-filename = '/home/bayrakrg/neurdy/d3/server_data/braincolor.csv'  # SLANT labels
+fatlas = '../data/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_2mm.nii.gz'  # Shaefer atlas
+satlas = '../data/mni_icbm152_t1_tal_nlin_asym_09c_seg_ds.nii.gz'  # SLANT atlas
+filename = '../data/braincolor.csv'  # SLANT labels
 conn_norm, mask, fun_atlas, struct_atlas, masked, id_to_name = prep_data(mat_filename, fatlas, satlas, filename)
 ##############################
 
